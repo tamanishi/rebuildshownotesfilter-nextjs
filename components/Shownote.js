f3b293bb0c636211ed9969303918feef55b0cd91
Masayuki Sunahara <tamanishi@gmail.com>
2021-02-28 15:28:47 +0900

    replaceAll -> replace

--- components/Shownote.js
+++ components/Shownote.js
@@ -3,6 +3,6 @@ import { escape, unescape } from 'html-escaper';
 
 export default function Shownote({ shownote, query }) {
   return (
-    <li><a href={ shownote.url } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ query ] } textToHighlight={ unescape(shownote.title).replaceAll('&nbsp;', ' ') } /></a></li>
+    <li><a href={ shownote.url } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ query ] } textToHighlight={ unescape(shownote.title).replace(/&nbsp;/g, ' ') } /></a></li>
   );
 }
