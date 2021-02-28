cc555f05ebf4637ed655838af4af7b516f5b926e
Masayuki Sunahara <tamanishi@gmail.com>
2021-02-28 15:24:18 +0900

    replaceAll -> replace

--- components/Episode.js
+++ components/Episode.js
@@ -6,7 +6,7 @@ import { escape, unescape } from 'html-escaper';
 export default function Episode({ episode, query }) {
   return (
     <div className='episode'>
-      <span className='epititle'><a href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ query ] } textToHighlight={ unescape(episode.title).replaceAll('&nbsp;', ' ') } /></a></span> <span className='pubdate'>(<Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment>)</span>
+      <span className='epititle'><a href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ query ] } textToHighlight={ unescape(episode.title).replace(/&nbsp;/g, ' ') } /></a></span> <span className='pubdate'>(<Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment>)</span>
       <span>{ episode.shownotes.map((shownote, i) => <Shownote shownote={ shownote } query={ query } key={ i } />)}</span>
     </div>
   );
