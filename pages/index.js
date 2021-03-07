49fe564556ac096ef966552b47f765da22ab8bc5
Masayuki Sunahara <tamanishi@gmail.com>
2021-03-07 23:17:00 +0900

    typo

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-]import Head from 'next/head'
+import Head from 'next/head'
 import Episode from '../components/Episode'
 import Header from '../components/Header'
 import { useState, useEffect, useRef } from 'react'
