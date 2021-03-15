75af39189620bfd9532a51e01ac754a243c1a55c
Masayuki Sunahara <tamanishi@gmail.com>
2021-03-15 21:41:33 +0900

    change endpoint to go implement

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ import { Container, Row, Col, Form } from 'react-bootstrap'
 import { escape, unescape } from 'html-escaper'
 
 async function getEpisodes() {
-  const endpoint = `https://rebuild-shownotes-json.tamanishi.workers.dev/`
+  const endpoint = `https://raw.githubusercontent.com/tamanishi/rebuild_feed_to_json_go/master/episodes.json`
+  // const endpoint = `https://rebuild-shownotes-json.tamanishi.workers.dev/`
   const res = await fetch(endpoint)
   const json = await res.json()
   return json.episodes
