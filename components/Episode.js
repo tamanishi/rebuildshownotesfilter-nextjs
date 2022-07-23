38048cd81003be3b89d2bea868c199a0f63f9b6d
Masayuki Sunahara <tamanishi@gmail.com>
2022-07-23 18:08:20 +0900

    add span tag

--- components/Episode.js
+++ components/Episode.js
@@ -13,7 +13,7 @@ export default function Episode({ episode, query }) {
 
     return (
     <div className='episode'>
-      <a className='epititle' href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ escapeStringRegexp(query) ] } textToHighlight={ unescape(episode.title).replace(/&nbsp;/g, ' ') } /></a> <span className='pubdate'>({isClient ? <Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment> : ''})</span>
+      <span className='epititle'><a href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ escapeStringRegexp(query) ] } textToHighlight={ unescape(episode.title).replace(/&nbsp;/g, ' ') } /></a></span> <span className='pubdate'>({isClient ? <Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment> : ''})</span>
       <ul>{ episode.shownotes.map((shownote, i) => <Shownote shownote={ shownote } query={ query } key={ i } />)}</ul>
     </div>
   );
