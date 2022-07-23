576e1471b2972c18f34ced64d35740c2dc56797c
Masayuki Sunahara <tamanishi@gmail.com>
2022-07-23 17:29:43 +0900

    swich date rendering

--- components/Episode.js
+++ components/Episode.js
@@ -1,15 +1,19 @@
 import Shownote from './Shownote'
+import { useState, useEffect } from 'react'
 import Moment from 'react-moment'
 import Highlighter from  'react-highlight-words'
 import { escape, unescape } from 'html-escaper';
 import escapeStringRegexp from 'escape-string-regexp'
 
 export default function Episode({ episode, query }) {
-  console.log(episode.publicationDate)
-  return (
+  const [isClient, setIsClient] = useState(false);  
+  useEffect(() => setIsClient(true), []);
+
+    return (
     <div className='episode'>
       <a className='epititle' href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ escapeStringRegexp(query) ] } textToHighlight={ unescape(episode.title).replace(/&nbsp;/g, ' ') } /></a> 
-      <span className='pubdate' suppressHydrationWarning={ true }>(<Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment>)</span>
+      (<Moment format='YYYY/MM/DD'>{ isClient ? episode.publicationDate : '' }</Moment>)
+      {/* <span className='pubdate'>(<time dateTime={ episode.publicationDate }>{isClient ? hhhhmmdd(dt) : ''}</time>)</span> */}
       <ul>{ episode.shownotes.map((shownote, i) => <Shownote shownote={ shownote } query={ query } key={ i } />)}</ul>
     </div>
   );
