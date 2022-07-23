2cff66eb516e87d4a3c3593b739995364f6e0c53
Masayuki Sunahara <tamanishi@gmail.com>
2022-07-23 16:50:39 +0900

    show raw publicationDate value

--- components/Episode.js
+++ components/Episode.js
@@ -5,10 +5,12 @@ import { escape, unescape } from 'html-escaper';
 import escapeStringRegexp from 'escape-string-regexp'
 
 export default function Episode({ episode, query }) {
+  console.log(episode.publicationDate)
   return (
     <div className='episode'>
       <a className='epititle' href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ escapeStringRegexp(query) ] } textToHighlight={ unescape(episode.title).replace(/&nbsp;/g, ' ') } /></a> 
       {/* <span className='pubdate'>(<Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment>)</span> */}
+      <span className='pubdate'>(episode.publicationDate)</span>
       <ul>{ episode.shownotes.map((shownote, i) => <Shownote shownote={ shownote } query={ query } key={ i } />)}</ul>
     </div>
   );
