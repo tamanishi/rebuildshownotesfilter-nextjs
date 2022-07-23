2896c5b64934cd4adbfae87cc55aa396cde7a96b
Masayuki Sunahara <tamanishi@gmail.com>
2022-07-23 18:01:55 +0900

    tweak

--- components/Episode.js
+++ components/Episode.js
@@ -8,14 +8,12 @@ import escapeStringRegexp from 'escape-string-regexp'
 export default function Episode({ episode, query }) {
   // see https://github.com/vercel/next.js/discussions/38263
   // see https://zenn.dev/noonworks/scraps/2114c8bab9a66e
-  const pubDate = new Date(episode.PublicationDate)
-    
   const [isClient, setIsClient] = useState(false)
   useEffect(() => setIsClient(true), []);
 
     return (
     <div className='episode'>
-      <a className='epititle' href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ escapeStringRegexp(query) ] } textToHighlight={ unescape(episode.title).replace(/&nbsp;/g, ' ') } /></a> ({isClient ? <Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment> : ''})
+      <a className='epititle' href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ escapeStringRegexp(query) ] } textToHighlight={ unescape(episode.title).replace(/&nbsp;/g, ' ') } /></a> <span className='pubdate'>({isClient ? <Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment> : ''})</span>
       <ul>{ episode.shownotes.map((shownote, i) => <Shownote shownote={ shownote } query={ query } key={ i } />)}</ul>
     </div>
   );
