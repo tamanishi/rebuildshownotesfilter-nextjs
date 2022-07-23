c18cd2f2f36d67d03e799b370bcdcae9e128543f
Masayuki Sunahara <tamanishi@gmail.com>
2022-07-23 16:42:31 +0900

    remove pulicationDate

--- components/Episode.js
+++ components/Episode.js
@@ -7,7 +7,8 @@ import escapeStringRegexp from 'escape-string-regexp'
 export default function Episode({ episode, query }) {
   return (
     <div className='episode'>
-      <a className='epititle' href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ escapeStringRegexp(query) ] } textToHighlight={ unescape(episode.title).replace(/&nbsp;/g, ' ') } /></a> <span className='pubdate'>(<Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment>)</span>
+      <a className='epititle' href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ escapeStringRegexp(query) ] } textToHighlight={ unescape(episode.title).replace(/&nbsp;/g, ' ') } /></a> 
+      {/* <span className='pubdate'>(<Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment>)</span> */}
       <ul>{ episode.shownotes.map((shownote, i) => <Shownote shownote={ shownote } query={ query } key={ i } />)}</ul>
     </div>
   );
