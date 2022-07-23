5d2ae9a5796f9f3d29aeb7df6c26585f1e7ab308
Masayuki Sunahara <tamanishi@gmail.com>
2022-07-23 16:32:52 +0900

    remove unnecessary span tag

--- components/Episode.js
+++ components/Episode.js
@@ -7,7 +7,7 @@ import escapeStringRegexp from 'escape-string-regexp'
 export default function Episode({ episode, query }) {
   return (
     <div className='episode'>
-      <span className='epititle'><a href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ escapeStringRegexp(query) ] } textToHighlight={ unescape(episode.title).replace(/&nbsp;/g, ' ') } /></a></span> <span className='pubdate'>(<Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment>)</span>
+      <a className='epititle' href={ episode.mediaUrl } target='_blank' rel='noopner noreferrer'><Highlighter highlightClassName='highlight' searchWords={ [ escapeStringRegexp(query) ] } textToHighlight={ unescape(episode.title).replace(/&nbsp;/g, ' ') } /></a> <span className='pubdate'>(<Moment format='YYYY/MM/DD'>{ episode.publicationDate }</Moment>)</span>
       <ul>{ episode.shownotes.map((shownote, i) => <Shownote shownote={ shownote } query={ query } key={ i } />)}</ul>
     </div>
   );
